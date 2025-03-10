4f287b39a36ba287960472dd06e0d0ba6e6c2a30
Vlad islav <vladfedevg@gmail.com>
2025-03-10 10:38:50 +0500

    v1.0.1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+const repoName = 'TODO-LIST'; // Замени на название своего репозитория
+
 export default defineConfig({
   plugins: [react()],
+  base: `/${repoName}/`,  // Добавляем / в начале и / в конце
+  build: {
+    outDir: 'dist', // Укажи, если у тебя другой каталог вывода
+  },
 })
